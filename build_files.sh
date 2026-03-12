78c508afb5475806449f6cc5822d85c79433aea4
gjhollmann <gjhollmann@gmail.com>
2026-03-12 16:10:39 -0700

    Fix Vercel Files

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,12 @@
 # Install dependencies
-python3 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Collect static files
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 # Create Vercel-compatible output vercel directory
 mkdir -p .vercel/output/static
 cp -r staticfiles/ .vercel/output/static/
 
-python3 manage.py makemigrations
-python3 manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
