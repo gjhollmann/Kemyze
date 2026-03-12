74e49ee1019b7c82c4f9e2bbec8b09fbefa0f2d9
gjhollmann <gjhollmann@gmail.com>
2026-03-12 16:42:15 -0700

    Fix Vercel Files

--- kemyze/urls.py
+++ kemyze/urls.py
@@ -40,7 +40,5 @@ router.register(r"users", UserViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path("", include(router.urls)),
     path('', include('tester.urls')),
-    path('admin/', admin.site.urls),
 ]
