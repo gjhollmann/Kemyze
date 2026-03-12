cc3022ed93a0106c85f514228e837d0768bc8c02
gjhollmann <gjhollmann@gmail.com>
2026-03-12 16:40:50 -0700

    Fix Vercel Files

--- kemyze/urls.py
+++ kemyze/urls.py
@@ -42,6 +42,5 @@ router.register(r"users", UserViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path('', include('tester.urls')),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path('admin/', admin.site.urls),
 ]
