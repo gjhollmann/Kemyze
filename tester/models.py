a6e033ae6fa6f458e0cb114361273e19048fd88d
gjhollmann <gjhollmann@gmail.com>
2026-03-14 16:33:54 -0700

    Connect Django to Oracle Heatwave DB

--- tester/models.py
+++ tester/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class TestTable(models.Model):
+    testName = models.CharField(max_length=255)
+    testPhrase = models.CharField(max_length=255)
