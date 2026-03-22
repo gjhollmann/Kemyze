85479db768c97917b00709a0407f26f05a38055a
Joshua Zamora <joshuazamora@csus.edu>
2026-03-22 05:34:14 -0700

    KM-16 Created serializer, API view, and route for all containers endpoint

--- tester/urls.py
+++ tester/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.tester, name='tester'),
-    path('dbtest', views.dbtester, name='dbtester')
+    path('dbtest', views.dbtester, name='dbtester'),
+    path('containers/', views.get_all_containers, name='get_all_containers'),
 ]
--- tester/views.py
+++ tester/views.py
@@ -1,6 +1,12 @@
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import permission_classes
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import TestTable
+from .models import Container
+from .serializers import ContainerSerializer
 
 def tester(request):
     return HttpResponse("Hello from Kemyze")
@@ -8,3 +14,10 @@ def tester(request):
 def dbtester(request):
     myData = TestTable.objects.all().values()
     return HttpResponse(myData)
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_containers(request):
+    containers = Container.objects.all()
+    serializer = ContainerSerializer(containers, many=True)
+    return Response(serializer.data)
