0dba70ad01fcaec852df7cee030a22c0fb131c3c
Eric Cheyssial <echeyssial@csus.edu>
2026-03-21 21:31:20 -0700

    Add update statements to modify user records.

--- db-testing/ec-users-testing.sql
+++ db-testing/ec-users-testing.sql
@@ -1,6 +1,11 @@
 -- Eric Cheyssial | Sprint 2 | Kemyze
 
--- KM-32
+-- KM-32: Insert User Records
+-- Return rows, evaluate schema (primary access at top). 
+SELECT * 
+FROM kemyzeDatabase.users
+ORDER BY access_level ASC;
+
 -- Insert records for users of each access level.
 INSERT INTO kemyzeDatabase.users (
 	user_id, 
@@ -21,9 +26,18 @@ VALUES
     (09873243, 'Stacy', 'Reyes', 'stre4@newmail.com', '9998887777', '546hg&%$#utr', 4, 1),
     (445568902, 'Bill', 'Roberts', 'billr99@mail.com', '3456210987', '405hujnds#@$%', 4, 1);
 
--- Return rows, verify insert correctness. 
-SELECT * 
-FROM kemyzeDatabase.users;
+-- KM-33: Update User Records
+UPDATE kemyzeDatabase.users
+SET email = 'bbroussard414@mail.com', phone = '9087652133'
+WHERE user_id = 734209;
+
+UPDATE kemyzeDatabase.users
+SET phone = '8972340000', last_name = 'Adams'
+WHERE user_id = 14578223;
+
+UPDATE kemyzeDatabase.users
+SET email = 'cadams44@newmail.com', phone = '9897632411'
+WHERE user_id = 216789;
 
 
 
