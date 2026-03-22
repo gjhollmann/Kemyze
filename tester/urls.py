df47138b3233489055399d32159f1ce853d3ccb9
Joshua Zamora <joshuazamora@csus.edu>
2026-03-22 05:51:34 -0700

    KM-17 Created container search endpoint with query filtering

--- tester/urls.py
+++ tester/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.tester, name='tester'),
     path('dbtest', views.dbtester, name='dbtester'),
     path('containers/', views.get_all_containers, name='get_all_containers'),
+    path('search/', views.search_containers, name='search_containers'),
 ]
--- tester/views.py
+++ tester/views.py
@@ -4,20 +4,39 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 from .models import TestTable
 from .models import Container
 from .serializers import ContainerSerializer
 
+
 def tester(request):
     return HttpResponse("Hello from Kemyze")
+
 
 def dbtester(request):
     myData = TestTable.objects.all().values()
     return HttpResponse(myData)
 
+
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def get_all_containers(request):
     containers = Container.objects.all()
+    serializer = ContainerSerializer(containers, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def search_containers(request):
+    query = request.GET.get('q', '')
+
+    containers = Container.objects.filter(
+        Q(chemical_name__icontains=query) |
+        Q(cas_number__icontains=query) |
+        Q(location__icontains=query)
+    )
+
     serializer = ContainerSerializer(containers, many=True)
     return Response(serializer.data)
