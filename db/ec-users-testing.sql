fb9ed4906779b1e7b7c433077e08d01e3cecf4c6
Eric Cheyssial <echeyssial@csus.edu>
2026-03-21 19:43:59 -0700

    Add db folder.

---
+++ db/ec-users-testing.sql
@@ -0,0 +1,29 @@
+-- Eric Cheyssial | Sprint 2 | Kemyze | KM-32
+
+-- Return rows, evaluate schema first. 
+SELECT * 
+FROM kemyzeDatabase.users;
+
+INSERT INTO users (
+	user_id, 
+    first_name, 
+    last_name, 
+    email, 
+    phone, 
+    password,
+    access_level,
+    location_id
+) 
+VALUES 
+	(976432, 'Alice', 'Robin', 'something1@mail.com', '0009567890', 'DCX342&ghd', 
+
+
+
+
+
+
+
+
+
+
+
