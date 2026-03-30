2bbaf768b1f5816498598866cb03d29804ff4b28
gjhollmann <gjhollmann@gmail.com>
2026-03-30 14:20:14 -0700

    Added Login app and frontend directory

--- kemyze/urls.py
+++ kemyze/urls.py
@@ -40,7 +40,7 @@ router.register(r"users", UserViewSet)
 # Wire up our API using automatic URL routing.
 
 urlpatterns = [
-    path('login/', include('apps.login.urls')),
+    path('login/', include('login.urls')),
     path('', include('tester.urls'))
 
 ]
