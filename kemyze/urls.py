e9a422d90b98c9c1365874475eb439be80edc539
gjhollmann <gjhollmann@gmail.com>
2026-03-30 13:56:48 -0700

    Added Login app and frontend directory

--- kemyze/urls.py
+++ kemyze/urls.py
@@ -41,5 +41,5 @@ router.register(r"users", UserViewSet)
 
 urlpatterns = [
     path('', include('tester.urls')),
-    path('', include('login.urls'))
+    path('login', include('login.urls'))
 ]
--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.tester, name='tester'),
+    path('', views.tester, name='tester'),
 ]
