f9f96c9e2f6fb06cedb517187b131b2bf832c266
george hollmann <gjhollmann@gmail.com>
2026-04-26 17:33:07 -0700

    implement Eric's work sprint 4

--- frontEnd/src/app/Pages/scanner.tsx
+++ frontEnd/src/app/Pages/scanner.tsx
@@ -1,8 +1,33 @@
+import {
+  Image,
+  ScrollView,
+  Text,
+  useWindowDimensions,
+  Alert,
+  View,
+  StyleSheet,
+  TextInput,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+} from "react-native";
 import { CameraView, CameraType, useCameraPermissions, Camera } from 'expo-camera'
 import { useEffect, useState } from 'react';
-import { View, Text, Alert, SafeAreaView, StyleSheet } from 'react-native';
+import { ScanPopup } from "../../../components/ScanPopup";
+import { handleContainerResponse } from "../../../utils/ScanResUtils";
+import { openBase64Pdf } from "../../../utils/PDFUtils";
+import { Linking } from "react-native";
+import GradientButton from "../../../components/GradientButton";
 
 export default function Scanner() {
+    
+    //Constanst for popup
+    const [popupVisible, setPopupVisible] = useState(false);
+    const [popupData, setPopupData] = useState<any>(null);
+    const [editPrivilege, setEditPrivilege] = useState(false);
+    const [currentKemId, setCurrentKemId] = useState<string | null>(null);
+    const [scannedKemID, setScannedKemId] = useState(0);
+
   const [permission, requestPermission] = useCameraPermissions();
   const [scanned, setScanned] = useState(false);
   const [scannedData, setScannedData] = useState<string | null>(null);
@@ -88,7 +113,100 @@ export default function Scanner() {
       </SafeAreaView>
     );
   }
+    
+    const showPopup = (title: string, message: string) => {
+      if (Platform.OS === "web") {
+        window.alert(`${title}\n\n${message}`);
+      } else {
+        Alert.alert(title, message);
+      }
+    };
 
+    // Helper to ensure users of access level <= 4 can edit info.
+    const quaternaryUser = 4;
+    
+    const canEditFromAccessLevel = (accessLevel: number) => {
+      return accessLevel <= quaternaryUser;
+    };
+
+    const fetchContainerData = async () => {
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => controller.abort(), 10000);
+
+      // Test with access level matched to query.
+      let quinaryUser = 5;
+      let accessLevel = 5;
+      let canEdit = canEditFromAccessLevel(accessLevel);
+      const getContainerUrl = "https://kemyze.vercel.app/containers/getContainer?kemID="+currentKemId+"&accessLevel=1";
+      try {
+          console.log(getContainerUrl);
+          const containerResponse = await fetch(getContainerUrl,
+            {
+              method: "GET",
+              signal: controller.signal,
+            }
+          );
+
+          clearTimeout(timeoutId);
+
+          // Alert if kemID or kemID and accessLevel are missing.
+          if (!containerResponse.ok) {
+              showPopup("Error", "The container does not exist or the request was invalid.");
+              return;
+          }
+      
+          const data = await containerResponse.json();
+          const result = handleContainerResponse(data);
+      
+          if (result.resType === "invalid") {
+              setEditPrivilege(false);
+              showPopup("Invalid Information", result.message);
+          
+          } else if (result.resType === "sds_only") {
+              setEditPrivilege(false);
+              await openBase64Pdf(result.pdfBase64);
+          
+          } else if (result.resType === "all_info") {
+              setPopupData(result.popupData);
+              setCurrentKemId(result.popupData.id);
+              setEditPrivilege(canEdit);
+              setPopupVisible(true);
+          
+          } else {
+              showPopup("Error", "Unexpected response.");
+          
+          }
+      
+      } catch (error: any) {
+          if (error.name === "AbortError") {
+              showPopup("Request timed out", "The server took too long to respond.");
+        
+          } else if (error instanceof SyntaxError) {
+              showPopup("Invalid response", "The server returned malfomed data.");
+        
+          } else {
+              showPopup("Network error", "Unable to reach server.");
+          
+          }
+      } // try ...
+    }
+
+    // Respond to press on 'View SDS.'
+    const handleViewSds = async () => {
+      if (!currentKemId) {
+        showPopup("Error", "No container ID is available for this SDS.");
+        return;
+      }
+
+      const sdsUrl = 'https://kemyze.vercel.app/containers/getSDS?kemID='+currentKemId; // Use current passed container ID.
+
+      try {
+        await Linking.openURL(sdsUrl);
+      } catch {
+        showPopup("Error", "Unable to open the SDS.");
+      }
+    };
+    
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.backgroundGradient}>
@@ -108,7 +226,29 @@ export default function Scanner() {
         <Text style={styles.QRScannerInstructions}>
           Scan a QR label to view the Safety Data Sheet (SDS)
         </Text>
+          <View style={styles.inputGroup}>
+            <View style={styles.inputWrapper}>
+              <TextInput
+                style={styles.input}
+                value={currentKemId}
+                onChangeText={setCurrentKemId}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                placeholderTextColor="rgba(255,255,255,0.3)"
+              />
+            </View>
+          </View>
+          <View style= {styles.buttonWrapper}>
+            <GradientButton title="Search" onPress={fetchContainerData} width="100%"/>
+          </View>
       </View>
+          <ScanPopup
+            visible={popupVisible}
+            onClose={() => setPopupVisible(false)}
+            scanResult={popupData}
+            editPrivilege={editPrivilege}
+            onViewSds={handleViewSds}
+          />
     </SafeAreaView>
   );
 }
@@ -147,10 +287,34 @@ export default function Scanner() {
       textAlign: "center",
       marginBottom: 8,
     },
+      inputWrapper: {
+        height: 50,
+        width: "75%",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        borderRadius: 25,
+        borderWidth: 1,
+        borderColor: "#334155",
+        alignSelf: "center",
+        justifyContent: "center",
+        paddingHorizontal: 20,
+      },
+      input: {
+        color: "white",
+        fontSize: 16,
+        height: "100%",
+      },
+      inputGroup: {
+        marginBottom: 20,
+      },
     QRScannerInstructions: {
       color: "#FFFFFF",
       fontFamily: "JetBrains Mono",
       textAlign: "center",
       marginTop: 40,
-    }
+    },
+      buttonWrapper: {
+          alignSelf: "center",
+          justifyContent: "center",
+          width: "100%"
+      }
   });
