7057c5926b9731508e213cd4861510c725528f1f
gjhollmann <gjhollmann@gmail.com>
2026-09-01 14:31:37 -0700

    Fix filter for getSearch

--- containers/views.py
+++ containers/views.py
@@ -134,7 +134,7 @@ def getSearch(request):
         if count == None:
             count = 0
         try:
-            FoundSearch = Containers.objects.filter(container_id__icontains=input) | Containers.objects.filter(chemical_name__icontains=input) | Containers.objects.filter(location__icontains=input)
+            FoundSearch = Containers.objects.filter(container_id__icontains=input) | Containers.objects.filter(chemical_name__icontains=input)
             data = serializers.serialize('json', FoundSearch)
             return JsonResponse(data)
         except Exception as error:
